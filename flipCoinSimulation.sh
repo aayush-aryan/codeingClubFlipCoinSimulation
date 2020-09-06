88b70ef8b612f0cd016c0e147e5a28202b515e45
unknown <log2aayusharyan@gmail.com>
2020-09-06 08:52:16 -0700

    finding total number of times head and tails win

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,12 +1,23 @@
 #!/bin/bash -x
 
 echo WELCOME TO FLIP COIN SIMULATOR
-check=1
-random=$((RANDOM%2))
-if (( random == check ))
-then
-	echo HEAD
-else
-	echo TAIL
-fi
+isHead=0;
+isHeadWon=0;
+isTailWon=0;
 
+for (( count=0; count<=10; count++ ))
+do
+	randomCheck1=$(( RANDOM%2 ))
+
+if [[ isHead -eq randomCheck1 ]]
+then
+	echo "Head"
+	isHeadWon=$(( $isHeadWon + 1 ))
+else
+	echo "Tail"
+	isTailWon=$(( $isTailWon + 1 ))
+fi
+done
+
+echo "Total Number Of Head Win is:  $isHeadWon"
+echo "Total Number Of Tail Win is: $isTailWon"
